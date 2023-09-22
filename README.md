98f5f5c73aad27d90bae17eb4228b2ac924c28a8
jproff <jessicakproffitt@gmail.com>
2023-09-22 09:44:09 -0400

    all reqs met

--- README.md
+++ README.md
@@ -21,11 +21,11 @@ SO THAT I can manage my time effectively
 ```md
 GIVEN I am using a daily planner to create a schedule
 WHEN I open the planner
-THEN the current day is displayed at the top of the calendar --
+THEN the current day is displayed at the top of the calendar
 WHEN I scroll down
-THEN I am presented with timeblocks for standard business hours of 9am&ndash;5pm --
+THEN I am presented with timeblocks for standard business hours of 9am&ndash;5pm
 WHEN I view the timeblocks for that day
-THEN each timeblock is color coded to indicate whether it is in the past, present, or future --
+THEN each timeblock is color coded to indicate whether it is in the past, present, or future
 WHEN I click into a timeblock
 THEN I can enter an event
 WHEN I click the save button for that timeblock
@@ -35,10 +35,7 @@ THEN the saved events persist
 ```
 
 The following animation demonstrates the application functionality:
-
-<!-- @TODO: create ticket to review/update image) -->
-
-![A user clicks on slots on the color-coded calendar and edits the events.](./Assets/05-third-party-apis-homework-demo.gif)
+![A user clicks on slots on the color-coded calendar and edits the events.](./assets/screenshotMod5.png)
 
 ## Grading Requirements
 
--- script.js
+++ script.js
@@ -1,16 +1,11 @@
 var busyTime = localStorage.getItem('busy');
-
-// TODO: change to below when done-
-// var currentHour = parseInt(dayjs().format('H'));
-var currentHour = 11;
+var currentHour = parseInt(dayjs().format('H'));
 if (localStorage.getItem('busy') == null) {
 	var busyObj = [];
 } else {
 	var busyObj = JSON.parse(localStorage.getItem('busy'));
 }
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+
 $(document).ready(function () {
 	var hourEl = document.getElementsByClassName('row time-block');
 	for (var i = 0; i < hourEl.length; i++) {
@@ -21,14 +16,7 @@ $(document).ready(function () {
 	if (busyObj != []) {
 		fillCalendar();
 	}
-	// TODO: Add a listener for click events on the save button. This code should
-	// use the id in the containing time-block as a key to save the user input in
-	// local storage. HINT: What does `this` reference in the click listener
-	// function? How can DOM traversal be used to get the "hour-x" id of the
-	// time-block containing the button that was clicked? How might the id be
-	// useful when saving the description in local storage?
-	//
-	// TODO: Add code to apply the past, present, or future class to each time
+
 	function canBeScheduled(currentHour, divHr) {
 		var hourBlockEl = document.getElementById('hour-' + divHr);
 
@@ -41,30 +29,26 @@ $(document).ready(function () {
 		}
 	}
 
-	// TODO: Add code to get any user input that was saved in localStorage and set
-	// the values of the corresponding textarea elements. HINT: How can the id
-	// attribute of each time-block be used to do this?
-
-	function fillCalendar() {
-		for (var i = 0; i < busyObj.length; i++) {
-			var text = JSON.stringify(busyObj[i]);
-			console.log(text);
-		}
-	}
-
 	function saveAttempt(event) {
 		event.preventDefault();
 		var btn = event.target.parentElement.id + 'txt';
 		var timeBlockTxt = document.getElementById(
 			event.target.parentElement.id + 'txt'
 		);
-		console.log(btn, timeBlockTxt.value);
 		if (timeBlockTxt.value !== ' ') {
 			busyObj.push({
 				[btn]: timeBlockTxt.value,
 			});
-			console.log(busyObj);
 			localStorage.setItem('busy', JSON.stringify(busyObj));
+		}
+	}
+
+	function fillCalendar() {
+		for (var i = 0; i < busyObj.length; i++) {
+			var text = JSON.stringify(busyObj[i]);
+			var key = Object.keys(busyObj[i])[0];
+			var value = Object.values(busyObj[i])[0];
+			document.getElementById(key).innerHTML = value;
 		}
 	}
 	// TODO: Add code to display the current date in the header of the page.
